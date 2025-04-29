1b79056b60059c7bf112548e978789b25dd32248
misaeio <144871387+misaeio@users.noreply.github.com>
2025-04-28 23:01:33 -0500

    Added 2 files. Csv and Cpp
    CSV file is the data set provided to us by the professor (approximately 50000 records). The Cpp file reads the CSV file searches and displays matching records. With an implemented cache

--- Source.cpp
+++ Source.cpp
@@ -1,111 +1,124 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <list>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
+#include <list>
+#include <string>
 
 using namespace std;
 
-class School {
-public:
-    string name;
-    string city;
-    int yearEstablished;
+struct CityKey {
+    string countryCode;
+    string cityName;
 
-    School(string n, string c, int y) : name(n), city(c), yearEstablished(y) {}
+    bool operator==(const CityKey& other) const {
+        return countryCode == other.countryCode && cityName == other.cityName;
+    }
 };
 
-class SchoolHashTable {
+struct CityKeyHash {
+    size_t operator()(const CityKey& key) const {
+        return hash<string>()(key.countryCode) ^ (hash<string>()(key.cityName) << 1);
+    }
+};
+
+class CityCache {
 private:
-    vector<list<School>> table; 
-    int tableSize;
-
-    int hashFunction(string key) {
-        int hash = 0;
-        for (char ch : key) {
-            hash += ch; 
-        }
-        return hash % tableSize; 
-    }
+    list<pair<CityKey, double>> cacheList;
+    unordered_map<CityKey, list<pair<CityKey, double>>::iterator, CityKeyHash> cacheMap;
+    const size_t maxSize = 10;
 
 public:
-    SchoolHashTable(int size) : tableSize(size) {
-        table.resize(tableSize);
-    }
-
-    void insert(School school) {
-        int index = hashFunction(school.name);
-        table[index].push_back(school);  
-    }
-
-    School* findByName(string name) {
-        int index = hashFunction(name);
-        for (School& school : table[index]) {
-            if (school.name == name) {
-                return &school;  
-            }
+    bool get(const CityKey& key, double& population) {
+        auto it = cacheMap.find(key);
+        if (it != cacheMap.end()) {
+        
+            cacheList.splice(cacheList.begin(), cacheList, it->second);
+            population = it->second->second;
+            return true;
         }
-        return nullptr; 
+        return false;
     }
 
-    void deleteByName(string name) {
-        int index = hashFunction(name);
-        auto& schools = table[index];
-        for (auto it = schools.begin(); it != schools.end(); ++it) {
-            if (it->name == name) {
-                schools.erase(it);  
-                return;
-            }
+    void put(const CityKey& key, double population) {
+        auto it = cacheMap.find(key);
+        if (it != cacheMap.end()) {
+            cacheList.erase(it->second);
+            cacheMap.erase(it);
+        }
+        cacheList.push_front({ key, population });
+        cacheMap[key] = cacheList.begin();
+
+        if (cacheList.size() > maxSize) {
+            auto last = cacheList.end();
+            last--;
+            cacheMap.erase(last->first);
+            cacheList.pop_back();
         }
     }
 
-    void display() {
-        for (int i = 0; i < tableSize; ++i) {
-            cout << "Index " << i << ": ";
-            for (const School& school : table[i]) {
-                cout << school.name << " (" << school.city << ", " << school.yearEstablished << ") ";
-            }
-            cout << endl;
+    void displayCache() {
+        cout << "Current Cache:\n";
+        for (auto& entry : cacheList) {
+            cout << entry.first.countryCode << ", " << entry.first.cityName
+                << " -> " << entry.second << endl;
         }
     }
 };
 
-int main() {
-    SchoolHashTable hashTable(3);
-
-    ifstream file("School.csv");
+double searchCSV(const string& filename, const CityKey& key) {
+    ifstream file(filename);
     string line;
+    getline(file, line); 
+
     while (getline(file, line)) {
         stringstream ss(line);
-        string name, city;
-        int year;
-        getline(ss, name, ',');
-        getline(ss, city, ',');
-        ss >> year;
-        hashTable.insert(School(name, city, year));
+        string countryCode, cityName, populationStr;
+        getline(ss, countryCode, ',');
+        getline(ss, cityName, ',');
+        getline(ss, populationStr, ',');
+
+        if (countryCode == key.countryCode && cityName == key.cityName) {
+            return stod(populationStr);
+        }
     }
+    return -1; 
+}
 
-    cout << "All schools in the table:" << endl;
-    hashTable.display();
+int main() {
+    string filename = "world_cities.csv";
+    CityCache cache;
 
-    string searchName;
-    cout << "\nEnter school name to search: ";
-    getline(cin, searchName);
-    School* foundSchool = hashTable.findByName(searchName);
-    if (foundSchool) {
-        cout << "Found school: " << foundSchool->name << " in " << foundSchool->city << endl;
+    while (true) {
+        cout << "\nEnter country code (or 'exit' to quit): ";
+        string countryCode;
+        cin >> countryCode;
+        if (countryCode == "exit") break;
+
+        cout << "Enter city name: ";
+        string cityName;
+        cin.ignore();
+        getline(cin, cityName);
+
+        CityKey key = { countryCode, cityName };
+        double population;
+
+        if (cache.get(key, population)) {
+            cout << "Population (from cache): " << population << endl;
+        }
+        else {
+            population = searchCSV(filename, key);
+            if (population != -1) {
+                cout << "Population (from file): " << population << endl;
+                cache.put(key, population);
+            }
+            else {
+                cout << "City not found.\n";
+            }
+        }
+
+        cache.displayCache(); 
     }
-    else {
-        cout << "School not found." << endl;
-    }
-
-    cout << "\nEnter school name to delete: ";
-    getline(cin, searchName);
-    hashTable.deleteByName(searchName);
-
-    cout << "\nSchools after deletion:" << endl;
-    hashTable.display();
 
     return 0;
 }
